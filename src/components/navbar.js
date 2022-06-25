4210cfb230c32d6feca29feabafcb63ca8b5026a
Afreen063 <afreen0501@gmail.com>
2022-06-25 22:20:04 +0530

    basic 2

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,8 +1,13 @@
 import React from "react";
 
 export default function Navbar(){
+    const color=[
+        <h1>hi</h1>,
+        <h2>bye</h2>//jxs array
+    ]
     return (
         <nav>
+            {color}
             <img src="https://tse1.mm.bing.net/th?id=OIP.SFIPCoCZwpzP7DxfMAf5NQHaCs&pid=Api&P=0&w=425&h=154" alt="hlt" className="nav" />
         </nav>
     )

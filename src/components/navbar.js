eacb45386f5bc7ba6996f947d59e9c026c153dab
Afreen063 <afreen0501@gmail.com>
2022-06-25 23:18:34 +0530

    basic 3

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
 
+
 export default function Navbar(){
     const color=[
         <h1>hi</h1>,
-        <h2>bye</h2>//jxs array
+        <h2>Afreen</h2>//jxs array
     ]
+    //console.log(list);
+    
     return (
         <nav>
             {color}
             <img src="https://tse1.mm.bing.net/th?id=OIP.SFIPCoCZwpzP7DxfMAf5NQHaCs&pid=Api&P=0&w=425&h=154" alt="hlt" className="nav" />
+        
         </nav>
     )
 }
---
+++ src/components/objlist.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        img:"https://tse4.mm.bing.net/th?id=OIP.HFNixjygsuGGQdQWjACZ5gHaEo&pid=Api&P=0&w=343&h=214", 
+        likes:"90",
+        comments:["good","bad"],
+        views:"50"
+    },
+    {
+        img :"https://wallpapercave.com/wp/HeLC3lH.jpg",
+       likes:"80",
+       comments:["good","bad"],
+       views:"20"
+    },
+    {
+        img:"http://getwallpapers.com/wallpaper/full/3/6/a/530610.jpg", 
+        likes:67,
+        comments:["good","bad"],
+        views:"10"
+    },
+    {
+        img:"https://downloadhdwallpapers.in/wp-content/uploads/2019/02/captain-marvels-4k-ultra-hd-wallpapers-3.jpg",
+        likes:"89",
+        comments:["good","bad"],
+        views:"80"
+    }
+]
+
+

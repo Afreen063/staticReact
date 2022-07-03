c7f8e4609fdbae22be79d79bc4e63fea9819f42a
Afreen063 <afreen0501@gmail.com>
2022-07-03 23:43:27 +0530

    making e-commerce card

---
+++ src/components copy/DealOfTheDay.js
@@ -0,0 +1,13 @@
+
+
+export default function Deal()
+{
+   
+    
+    return (
+     <div className="deal-card">
+         <img src="" alt="cut" />
+         <h1></h1>
+     </div>   
+    )
+}
---
+++ src/components copy/Navigation.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export default function Navi()
+{
+    return (
+        <div>
+             <ul>
+                <li>home
+                </li>
+                <li>men</li>
+                <li >women</li>
+                <li>
+                    kids
+                </li>
+                <li>searchbar</li>
+                <li>login</li>
+                <li>cart</li>
+             </ul>
+        </div>
+    )
+}
